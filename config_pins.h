6e810dee859835df61947560470a3e3f0cce59c5
Joel LOURENCO <joel.lourenco.perso@gmail.com>
2025-09-03 08:46:10 +0200

    Fix pins hp

--- config_pins.h
+++ config_pins.h
@@ -6,6 +6,6 @@
 #define I2S_PCM_DOUT  14
 
 // I²S → HP board (mêmes lignes, lib AudioI2S)
-#define I2S_SPK_BCLK  9
-#define I2S_SPK_LRCK  5
-#define I2S_SPK_DOUT  14
+#define I2S_SPK_BCLK  42
+#define I2S_SPK_LRCK  2
+#define I2S_SPK_DOUT  41
